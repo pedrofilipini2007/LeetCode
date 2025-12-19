88391a5a28fe34f51e36d0a6f0efa3dfd89a8ec3
Pedro Filipini <pedro.filipini42@gmail.com>
2025-12-19 11:06:02 -0300

    improve twoSum method using HashMap
    Refactored twoSum method to use a single HashMap for improved efficiency, replacing the brute force approach.

--- Easy/1-two-sum.java
+++ Easy/1-two-sum.java
@@ -1,20 +1,32 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-
-
-    for (int i = 0; i < nums.length; i++)
-    {
-        for (int j = 0; j < nums.length; j ++){
-
-            if (i == j) continue;
-
-            if (nums[i] + nums[j] == target){
-                int[] index = new int[]{i,j};
-                return index;
-            }
-
+public int[] twoSum(int[] nums, int target) {
+        //  try use only one Hashmap
+        Map<Integer,Integer> collect = new HashMap<>();
+        for (int i = 0; i< nums.length; i++)
+        {
+            if(collect.containsKey(target - nums[i])) return new int[] {i, collect.get(target - nums[i])};
+            collect.put(nums[i], i);
         }
+        return new int[] {};
+
     }
-    return null;
-    }
-}
+
+
+
+
+
+//    brute force method
+//    for (int i = 0; i < nums.length; i++)
+//    {
+//        for (int j = 0; j < nums.length; j ++){
+//
+//            if (i == j) continue;
+//
+//            if (nums[i] + nums[j] == target){
+//                int[] index = new int[]{i,j};
+//                return index;
+//            }
+//
+//        }
+//    }
+   // return null;
+//    }
